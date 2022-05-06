4b929adc2c632b58d8f3e3a27e7495e66d48f68b
Wassim <privat@wassim.no>
2022-05-06 16:14:56 +0200

    progress backup

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,54 @@ import React, { useState, useEffect } from 'react';
 import './styles/App.css';
 
 import TextInput from './modules/TextInput/TextInput';
-import Display from './modules/taskDisplay/taskDisplay';
+import TaskDisplay from './modules/TaskDisplay/TaskDisplay';
+import NewTaskButton from './modules/NewTaskButton/NewTaskButton';
 
-const tempValue = '12345';
+const tempValue = [1, 2, 3, 4, 5];
 
 function App() {
   const [displayText, setDisplayText] = useState('');
   const [textInputValue, setTextInputValue] = useState('');
 
-  // useEffect(() => {
-  //   setDisplayText(textInputValue);
-  // });
+  useEffect(() => {
+    displayText == textInputValue ? console.log('win') : console.log('not yet');
+  }, [textInputValue]);
 
   const handleTextInput = (e) => {
     setTextInputValue(e.target.value);
-    setDisplayText(e.target.value);
   };
+
+  const handleNewTaskButton = (e) => {
+    const getRandomCharacter = () => {
+      return tempValue[Math.floor(Math.random() * tempValue.length)];
+    };
+
+    let newTask = '';
+    for (let i = 0; i < 8; i++) {
+      console.log('ho');
+      newTask += getRandomCharacter();
+    }
+
+    setDisplayText(newTask);
+  };
+
+  // const randomCharacter = () => {
+  //   const newTask = tempValue[Math.floor(Math.random() * tempValue.length)];
+  // };
 
   return (
     <>
       <h1>Helloo World</h1>
-      <Display currentTask={tempValue} />
+      <TaskDisplay currentTask={displayText} />
       <br />
       <br />
       <TextInput
         valueState={textInputValue}
         handleTextInput={handleTextInput}
       />
+      <br />
+      <br />
+      <NewTaskButton onClick={handleNewTaskButton} />
     </>
   );
 }
---
+++ src/modules/NewTaskButton/NewTaskButton.tsx
@@ -0,0 +1,5 @@
+const NewTaskButton = (props) => {
+  return <button onClick={props.onClick}>New</button>;
+};
+
+export default NewTaskButton;
